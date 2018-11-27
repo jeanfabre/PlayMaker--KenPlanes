a8ee3fa8661f152e92eba0f71c1629dbd506f588
Jean Fabre <contact@fabrejean.net>
2018-11-27 09:03:38 +0300

    Initial commit

---
+++ Assets/PlayMaker Custom Actions/Input/GetKeyUpFromString.cs
@@ -0,0 +1,57 @@
+// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+// Made by Djaydino http://www.jinxtergames.com
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Input)]
+	[Tooltip("Gets the down state of the Keystring. Warning!! The String must be a valid KeyCode.")]
+	public class GetKeyDownFromString : FsmStateAction
+	{
+		[RequiredField]
+		[Tooltip("The keystring to test. Waring!! The String must be a valid KeyCode.")]
+		public FsmString keyCode;
+		
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		[Tooltip("Store if the key is down (True) or up (False).")]
+		public FsmBool storeResult;
+
+        [Tooltip("Repeat every frame. Useful if you're waiting for a key press/release.")]
+        public bool everyFrame;
+
+		private KeyCode key;
+		
+		public override void Reset()
+		{
+			keyCode = null;
+			storeResult = null;
+			everyFrame = false;
+		}
+		
+		public override void OnEnter()
+		{
+			key = (KeyCode)System.Enum.Parse(typeof(KeyCode), keyCode.Value) ;
+
+			DoGetKey();
+			
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+		
+
+		public override void OnUpdate()
+		{
+			DoGetKey();
+		}
+		
+		void DoGetKey()
+		{
+			storeResult.Value = Input.GetKeyDown(key);
+		}
+		
+	}
+}
